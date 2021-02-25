829ccff1a20065e3c74f6e6cd23f565197287742
chist <chist@DESKTOP-STJIHG7>
2021-02-25 09:54:18 -0500

    THIS PRACTICE PUSH

---
+++ AutomationProject/src/eComerceStore/Ecomerce.java
@@ -0,0 +1,29 @@
+package eComerceStore;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ecomerce {
+	
+	
+String game="football";
+	public static void main(String[] args) {
+		
+		Ecomerce game=new Ecomerce();
+		if(game.equals(game)) {
+			System.out.println("Right");
+		}
+	
+		
+		 int age=17;
+		
+		if (age<18) {
+			System.out.println("able to vote");
+		}
+//		System.setProperty("Webdriver.chrome.driver", "C:\\Users\\chist\\Downloads\\chromedriver_win32 (1)");
+//		WebDriver driver= new ChromeDriver();
+//		driver.get("https://j2store.net/demo/");
+
+	}
+
+}
